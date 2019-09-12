0e284cb6d24ae5a8ae4209f3d4e9e22ca48a406a
mcdruid <drew@mcdruid.co.uk>
2019-09-12 22:55:04 +0100

    Tidying up, improving debug output.

--- __init__.py
+++ __init__.py
@@ -26,79 +26,50 @@ import requests
 import pafy
 from bs4 import BeautifulSoup
 
-from mycroft.audio.services.vlc import VlcService
-
 from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
 from mycroft.skills.core import intent_file_handler
 from mycroft.util.log import LOG
-
+from mycroft.audio.services.vlc import VlcService
 from mycroft.audio import wait_while_speaking
 
 # Static values for search requests
-base_url = "https://www.youtube.com/results?search_query="
+base_url = 'https://www.youtube.com'
+search_url = base_url + '/results?search_query='
 
 class YoutubeSkill(CommonPlaySkill):
 
     def __init__(self):
-        super().__init__(name="YoutubeSkill")
+        super().__init__(name='YoutubeSkill')
 
-        self.audio_state = "stopped"  # 'playing', 'stopped'
+        self.audio_state = 'stopped'  # 'playing', 'stopped'
         self.station_name = None
         self.stream_url = None
         self.mpeg_url = None
         self.process = None
         self.regexes = {}
         self.mediaplayer = VlcService(config={'low_volume': 10, 'duck': True})
-        self.audio_state = "stopped"  # 'playing', 'stopped'
+        self.audio_state = 'stopped'  # 'playing', 'stopped'
 
     def CPS_match_query_phrase(self, phrase):
         # Look for regex matches starting from the most specific to the least
-
-        # Play <data> internet radio on tune in
-        #match = re.search(self.translate_regex('internet_radio_on_tunein'), phrase)
-        #if match:
-        #    data = re.sub(self.translate_regex('internet_radio_on_tunein'), '', phrase)
-        #    LOG.debug("CPS Match (internet_radio_on_tunein): " + data)
-        #    return phrase, CPSMatchLevel.EXACT, data
-        #
-        # Play <data> radio on tune in
-        #match = re.search(self.translate_regex('radio_on_tunein'), phrase)
-        #if match:
-        #    data = re.sub(self.translate_regex('radio_on_tunein'), '', phrase)
-        #    LOG.debug("CPS Match (radio_on_tunein): " + data)
-        #     return phrase, CPSMatchLevel.EXACT, data
 
         # Play <data> on youtube
         match = re.search(self.translate_regex('on_youtube'), phrase)
         if match:
             data = re.sub(self.translate_regex('on_youtube'), '', phrase)
-            LOG.debug("CPS Match (on_youtube): " + data)
+            LOG.debug('CPS Match (on_youtube): ' + data)
             return phrase, CPSMatchLevel.EXACT, data
-
-        # Play <data> internet radio
-        #match = re.search(self.translate_regex('internet_radio'), phrase)
-        #if match:
-        #    data = re.sub(self.translate_regex('internet_radio'), '', phrase)
-        #    LOG.debug("CPS Match (internet_radio): " + data)
-        #    return phrase, CPSMatchLevel.CATEGORY, data
-        #
-        ## Play <data> radio
-        #match = re.search(self.translate_regex('radio'), phrase)
-        #if match:
-        #    data = re.sub(self.translate_regex('radio'), '', phrase)
-        #    LOG.debug("CPS Match (radio): " + data)
-        #    return phrase, CPSMatchLevel.CATEGORY, data
 
         return phrase, CPSMatchLevel.GENERIC, phrase
 
     def CPS_start(self, phrase, data):
-        LOG.debug("CPS Start: " + data)
+        LOG.debug('CPS Start: ' + data)
         self.search_youtube(data)
 
     # Attempt to find the first result matching the query string
     def search_youtube(self, search_term):
         tracklist = []
-        res = requests.get(base_url + search_term)
+        res = requests.get(search_url + search_term)
         # TODO: check status code etc...
         html = res.content
         soup = BeautifulSoup(html, 'html.parser')
@@ -107,10 +78,10 @@ class YoutubeSkill(CommonPlaySkill):
             self.vid_url = vid['href']
             self.vid_name = vid.string
             self.stream_url = self.get_stream_url(self.vid_url)
-            self.audio_state = "playing"
-            self.speak_dialog("now.playing", {"station": self.vid_name} )
+            self.audio_state = 'playing'
+            self.speak_dialog('now.playing', {'content': self.vid_name} )
             wait_while_speaking()
-            LOG.debug("Found stream URL: " + self.vid_url)
+            LOG.debug('Found stream URL: ' + self.vid_url)
 
             tracklist.append(self.stream_url)
             self.mediaplayer.add_list(tracklist)
@@ -119,22 +90,25 @@ class YoutubeSkill(CommonPlaySkill):
             return
 
         # We didn't find any playable stations
-        self.speak_dialog("not.found")
+        self.speak_dialog('not.found')
         wait_while_speaking()
-        LOG.debug("Could not find any results with the query term: " + search_term)
+        LOG.debug('Could not find any results with the query term: ' + search_term)
 
     def get_stream_url(self, youtube_url):
-        LOG.debug('pafy processing: https://www.youtube.com' + youtube_url)
-        video = pafy.new('https://www.youtube.com' + youtube_url)
-        bestaudio = video.getbestaudio()
+        abs_url = base_url + youtube_url
+        LOG.debug('pafy processing: ' + abs_url)
+        streams = pafy.new(abs_url)
+        LOG.debug('audiostreams found: ' + str(streams.audiostreams));
+        bestaudio = streams.getbestaudio()
+        LOG.debug('audiostream selected: ' + str(bestaudio));
         return bestaudio.url
 
     def stop(self):
-        if self.audio_state == "playing":
+        if self.audio_state == 'playing':
             self.mediaplayer.stop()
             self.mediaplayer.clear_list()
-            LOG.debug("Stopping stream")
-        self.audio_state = "stopped"
+            LOG.debug('Stopping stream')
+        self.audio_state = 'stopped'
         self.station_name = None
         self.station_id = None
         self.stream_url = None
