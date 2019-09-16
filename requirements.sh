62f1cd97491f05b24925d458c9f6782fe05c57aa
mcdruid <drew@mcdruid.co.uk>
2019-09-16 23:06:27 +0100

    Install minimal vlc packages instead of the main one with a lot of dependencies.

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -yq vlc
+
+# see: https://www.raspberrypi.org/forums/viewtopic.php?t=245364
+sudo apt-get install -yq vlc-bin vlc-plugin-base
 exit 0
